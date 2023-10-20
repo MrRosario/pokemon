693dcd47eeb02a3e5f22c118c19814ff2651fe50
MrRosario <eugenio.dorosario94@gmail.com>
2023-10-20 17:09:14 -0300

    style: character style

--- src/pages/Map/styled.js
+++ src/pages/Map/styled.js
@@ -1,11 +1,30 @@
 import styled from "styled-components";
-
-import img from "assets/images/pageBackground.png";
+import pageBackgroundImg from "assets/images/pageBackground.png";
 
 export const MapWrapper = styled.div`
   position: relative;
-  background-image: url(${img});
+  background-image: url(${pageBackgroundImg});
   background-color: #5dae12;
   background-size: cover;
   height: 100vh;
+`;
+
+export const CharacterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const TooltipWrapper = styled.div`
+  height: 67px;
+`;
+
+export const Tooltip = styled.img``;
+
+export const CharacterFront = styled.img`
+  cursor: pointer;
+  position: relative;
+  right: 5px;
 `;
