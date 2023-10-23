5a9f1460bdd2fdbbba2a6c07931846ab0c58d55e
MrRosario <eugenio.dorosario94@gmail.com>
2023-10-23 17:15:18 -0300

    feat: utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,8 @@ export const randomNumberGenerator = (start, limit) =>
 
 export const findHpStats = (statsArr) =>
   statsArr.find((statObj) => statObj.stat.name === "hp");
+
+export const transformValue = (weight) => Number((weight / 100).toFixed(2));
+
+export const isCaptured = (pokemonArr, pokemonName) =>
+  pokemonArr.some((pokemon) => pokemon.name === pokemonName);
